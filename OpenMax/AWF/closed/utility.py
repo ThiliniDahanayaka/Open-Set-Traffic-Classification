c13cc75c327dd66950dd6d357e60cb92ef460b23
Thilini Dahanayaka <thilini.dahanayaka@eng.pdn.ac.lk>
2022-03-08 23:50:19 +1100

    Create utility.py

---
+++ OpenMax/AWF/closed/utility.py
@@ -0,0 +1,44 @@
+import pickle
+import numpy as np
+
+# Load data for non-defended dataset for CW setting
+def LoadDataNoDefCW():
+
+    print ("Loading non-defended dataset for closed-world scenario")
+    # Point to the directory storing data
+    dataset_dir ="/media/SATA_1/thilini_open_extra/final_datasets/AWF/"
+    # y represents a sequence of corresponding label (website's label)
+
+    # Load training data
+    X_train = np.load(dataset_dir+'X_train.npy')
+    X_train = X_train[:, 0:1500]
+    y_train = np.load(dataset_dir+'y_train.npy')
+    ind = np.where(y_train<200)[0]
+    X_train = X_train[ind]
+    y_train = y_train[ind]
+
+    # Load validation data
+    X_valid = np.load(dataset_dir+'X_valid.npy')
+    X_valid = X_valid[:, 0:1500]
+    y_valid = np.load(dataset_dir+'y_valid.npy')
+    ind = np.where(y_valid<200)[0]
+    X_valid = X_valid[ind]
+    y_valid = y_valid[ind]
+
+    # Load testing data
+    X_test = np.load(dataset_dir+'X_test.npy')
+    X_test = X_test[:, 0:1500]
+    y_test = np.load(dataset_dir+'y_test.npy')
+    ind = np.where(y_test<200)[0]
+    X_test = X_test[ind]
+    y_test = y_test[ind]
+
+    print ("Data dimensions:")
+    print ("X: Training data's shape : ", X_train.shape)
+    print ("y: Training data's shape : ", y_train.shape)
+    print ("X: Validation data's shape : ", X_valid.shape)
+    print ("y: Validation data's shape : ", y_valid.shape)
+    print ("X: Testing data's shape : ", X_test.shape)
+    print ("y: Testing data's shape : ", y_test.shape)
+
+    return X_train, y_train, X_valid, y_valid, X_test, y_test
