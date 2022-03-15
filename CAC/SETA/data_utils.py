fd56eb44bcfe00e37a3e1037d8e74aaf8a397344
Thilini Dahanayaka <thilini.dahanayaka@eng.pdn.ac.lk>
2022-03-15 23:27:45 +1100

    Add files via upload

---
+++ CAC/SETA/data_utils.py
@@ -0,0 +1,279 @@
+import torch
+# import torchvision
+# import torchvision.transforms as tf
+import json
+from torch.autograd import Variable
+from torch.utils.data import Dataset
+import numpy as np
+import random
+import pickle
+random.seed(1000)
+
+class customDataset(Dataset):
+    def __init__(self, data, target, shuffle=False):
+        self.data = torch.from_numpy(data).float()
+        self.target = torch.from_numpy(target).float()
+        self.shuffle = shuffle
+
+        if self.shuffle:
+        	ind=np.arange(self.data.shape[0])
+        	np.random.shuffle(ind)
+        	self.data = self.data[ind]
+        	self.target = self.target[ind]
+
+        
+    def __getitem__(self, index):
+        x = self.data[index]
+        y = self.target[index]
+        
+        
+        return x, y
+    
+    def __len__(self):
+        return len(self.data)
+
+def get_train_loaders(datasetName, trial_num, cfg):
+    """
+        Create training dataloaders.
+
+        datasetName: name of dataset
+        trial_num: trial number dictating known/unknown class split
+        cfg: config file
+
+        returns trainloader, evalloader, testloader, mapping - changes labels from original to known class label
+    """
+    trainSet, valSet, testSet, _ = load_datasets(datasetName, cfg, trial_num)
+    
+    # print(type(trainSet))
+    batch_size = cfg['batch_size']
+
+    # print(testSet.len)
+
+    trainloader = torch.utils.data.DataLoader(trainSet, batch_size=batch_size, shuffle=True, num_workers = 0)
+    valloader = torch.utils.data.DataLoader(valSet, batch_size=batch_size, shuffle=True, num_workers = 0)
+    testloader = torch.utils.data.DataLoader(testSet, batch_size=batch_size, shuffle=True, num_workers = 0)
+
+    mapping=None
+
+    # print(type(trainloader))
+
+    return trainloader, valloader, testloader, mapping
+
+def get_eval_loaders(datasetName, trial_num, cfg):
+    # print('*****************************************************************************************\n get_eval_loaders To be implemented\n ****************************************************************************************************')
+    _, _, testSet, unknownSet = load_datasets(datasetName, cfg, trial_num)
+
+    batch_size = cfg['batch_size']
+
+    knownloader = torch.utils.data.DataLoader(testSet, batch_size=batch_size, shuffle=False)
+    unknownloader = torch.utils.data.DataLoader(unknownSet, batch_size=batch_size, shuffle=False)
+
+    mapping=None
+
+    return knownloader, unknownloader, mapping
+
+
+
+def get_data_stats(dataset, known_classes):
+    print('*****************************************************************************************\n get_data_stats To be implemented\n ****************************************************************************************************')
+
+
+def load_datasets(datasetName, cfg_o, trial_num):
+    
+    datatpath = "/media/SATA_1/thilini_open_extra/final_datasets/SETA/"
+
+    with open("/media/SATA_1/thilini_open_extra/final_datasets/SETA/data_file.json") as config_file:
+        cfg = json.load(config_file)[trial_num]
+    closeset = cfg["closed"]
+    openset = cfg["open"]
+    
+        
+    # x = np.load(datatpath+'X_train.npy').astype('float32')
+    # y = np.load(datatpath+'y_train.npy').astype('float32')
+    with open(datatpath+'video_X_train.pkl', 'rb') as file:
+        x = pickle.load(file)
+    with open(datatpath+'video_y_train.pkl', 'rb') as file:
+        y = pickle.load(file)
+
+    y_new = np.ones((1,))
+    x_new = np.ones((1, x.shape[1]))
+
+    for count, val in enumerate(closeset):
+        ind = np.where(y==val)[0]
+        x_new = np.append(x_new, x[ind], axis=0)
+        # print(x_new.shape)
+        y_new = np.append(y_new, np.ones((len(ind),))*count)
+
+    x_new = x_new[1:]
+    y_new = y_new[1:]
+
+    del x, y
+
+    x_new = x_new.astype('float32')
+    y_new = y_new.astype('float32')
+
+    x_new = x_new[:, 0:500, np.newaxis]
+    x_new = np.swapaxes(x_new, 2, 1)
+    # trainSet = x, y
+
+    trainSet = customDataset(data=x_new, target=y_new, shuffle=True)
+
+    # x = np.load(datatpath+'X_valid.npy').astype('float32')
+    # y = np.load(datatpath+'y_valid.npy').astype('float32')
+
+    with open(datatpath+'video_X_valid.pkl', 'rb') as file:
+        x = pickle.load(file)
+    with open(datatpath+'video_y_valid.pkl', 'rb') as file:
+        y = pickle.load(file)
+
+    y_new = np.ones((1,))
+    x_new = np.ones((1, x.shape[1]))
+
+    for count, val in enumerate(closeset):
+        ind = np.where(y==val)[0]
+        x_new = np.append(x_new, x[ind], axis=0)
+        # print(x_new.shape)
+        y_new = np.append(y_new, np.ones((len(ind),))*count)
+
+    x_new = x_new[1:]
+    y_new = y_new[1:]
+
+    del x, y
+
+    x_new = x_new.astype('float32')
+    y_new = y_new.astype('float32')
+
+    x_new = x_new[:, 0:500, np.newaxis]
+    x_new = np.swapaxes(x_new, 2, 1)
+    valSet = customDataset(data=x_new, target=y_new)
+
+    # x = np.load(datatpath+'X_test.npy').astype('float32')
+    # y = np.load(datatpath+'y_test.npy').astype('float32')
+
+    with open(datatpath+'video_X_test.pkl', 'rb') as file:
+        x = pickle.load(file)
+    with open(datatpath+'video_y_test.pkl', 'rb') as file:
+        y = pickle.load(file)
+    # print(y.max(), y.min())
+    y_new = np.ones((1,))
+    x_new = np.ones((1, x.shape[1]))
+
+
+    for count, val in enumerate(closeset):
+        ind = np.where(y==val)[0]
+        x_new = np.append(x_new, x[ind], axis=0)
+        # print(x_new.shape)
+        y_new = np.append(y_new, np.ones((len(ind),))*count)
+
+    x_new = x_new[1:]
+    y_new = y_new[1:]
+
+    del x, y
+
+    x_new = x_new.astype('float32')
+    y_new = y_new.astype('float32')
+
+
+    x_new = x_new[:, 0:500, np.newaxis]
+    x_new = np.swapaxes(x_new, 2, 1)
+    # print(x_new.shape)
+    testSet = customDataset(data=x_new, target=y_new)
+
+
+    open_l = cfg_o['num_known_classes']
+    # x = np.load(datatpath+'X_open.npy').astype('float32')
+    with open(datatpath+'video_X_test.pkl', 'rb') as file:
+        x = pickle.load(file)
+    with open(datatpath+'video_y_test.pkl', 'rb') as file:
+        y = pickle.load(file)
+    # y = (np.ones((x.shape[0],))*open_l).astype('float32')
+
+    x_new = np.ones((1, x.shape[1]))
+
+    for count, val in enumerate(openset):
+        ind = np.where(y==val)[0]
+        x_new = np.append(x_new, x[ind][0:14], axis=0)
+        # print(x_new.shape)
+
+    x_new = x_new[1:]
+    y_new = np.ones((x_new.shape[0]))*open_l
+
+    del x, y
+
+    x_new = x_new.astype('float32')
+    y_new = y_new.astype('float32')
+
+
+    x_new = x_new[:, 0:500, np.newaxis]
+    x_new = np.swapaxes(x_new, 2, 1)
+    unknownSet = customDataset(data=x_new, target=y_new)
+
+    # unknownSet=None
+
+    return trainSet, valSet, testSet, unknownSet
+
+def get_anchor_loaders(datasetName, trial_num, cfg):
+    # print('*****************************************************************************************\n get_anchor_loaders To be implemented\n ****************************************************************************************************')
+    trainSet = load_anchor_datasets(datasetName, cfg, trial_num)
+    
+    # print(type(trainSet))
+    batch_size = cfg['batch_size']
+
+    trainloader = torch.utils.data.DataLoader(trainSet, batch_size=batch_size, shuffle=True, num_workers = 0)
+    # valloader = torch.utils.data.DataLoader(valSet, batch_size=batch_size, shuffle=True, num_workers = 0)
+    # testloader = torch.utils.data.DataLoader(testSet, batch_size=batch_size, shuffle=True, num_workers = 0)
+
+    # mapping=None
+
+    # print(type(trainloader))
+
+    return trainloader
+
+def load_anchor_datasets(datasetName, cfg_o, trial_num):
+    # print('*****************************************************************************************\n load_anchor_datasets To be implemented\n ****************************************************************************************************')
+    datatpath = "/media/SATA_1/thilini_open_extra/final_datasets/SETA/"
+
+    with open("/media/SATA_1/thilini_open_extra/final_datasets/SETA/data_file.json") as config_file:
+        cfg = json.load(config_file)[trial_num]
+    closeset = cfg["closed"]
+    openset = cfg["open"]
+    
+        
+    # x = np.load(datatpath+'X_train.npy').astype('float32')
+    # y = np.load(datatpath+'y_train.npy').astype('float32')
+    with open(datatpath+'video_X_train.pkl', 'rb') as file:
+        x = pickle.load(file)
+    with open(datatpath+'video_y_train.pkl', 'rb') as file:
+        y = pickle.load(file)
+
+    y_new = np.ones((1,))
+    x_new = np.ones((1, x.shape[1]))
+
+    for count, val in enumerate(closeset):
+        ind = np.where(y==val)[0]
+        x_new = np.append(x_new, x[ind], axis=0)
+        # print(x_new.shape)
+        y_new = np.append(y_new, np.ones((len(ind),))*count)
+
+    x_new = x_new[1:]
+    y_new = y_new[1:]
+
+    del x, y
+
+    x_new = x_new.astype('float32')
+    y_new = y_new.astype('float32')
+
+    x_new = x_new[:, 0:500, np.newaxis]
+    x_new = np.swapaxes(x_new, 2, 1)
+    # trainSet = x, y
+
+    trainSet = customDataset(data=x_new, target=y_new, shuffle=True)
+
+    return trainSet
+
+def create_dataSubsets(dataset, classes_to_use, idxs_to_use = None):
+    print('*****************************************************************************************\n create_dataSubsets To be implemented\n ****************************************************************************************************')
+
+
+def create_target_map(known_classes, num_classes):
+    print('*****************************************************************************************\n create_target_map To be implemented\n ****************************************************************************************************')
---
+++ CAC/SETA/networks/openSetClassifier.py
@@ -0,0 +1,164 @@
+"""
+	Network definition for our proposed CAC open set classifier. 
+
+	Dimity Miller, 2020
+"""
+
+
+import torch
+import torchvision
+import torch.nn as nn
+
+class openSetClassifier(nn.Module):
+	def __init__(self, num_classes = 20, init_weights = False, **kwargs):
+		super(openSetClassifier, self).__init__()
+
+		self.num_classes = num_classes
+		self.encoder = BaseEncoder(init_weights)
+		
+		# if im_size == 32:
+		# 	self.classify = nn.Linear(128*4*4, num_classes)
+		# elif im_size == 64:
+		# 	self.classify = nn.Linear(128*8*8, num_classes)
+		# else:
+		# 	print('That image size has not been implemented, sorry.')
+		# 	exit()
+		self.classify = nn.Linear(64, num_classes)
+
+		self.anchors = nn.Parameter(torch.zeros(self.num_classes, self.num_classes).double(), requires_grad = False)
+
+		# if init_weights:
+		# 	self._initialize_weights()
+		
+		self.cuda()
+
+
+	def forward(self, x, skip_distance = False):
+		batch_size = len(x)
+
+		x = self.encoder(x)
+		x = x.view(batch_size, -1)
+
+		outLinear = self.classify(x)
+
+		if skip_distance:
+			return outLinear, None
+
+		outDistance = self.distance_classifier(outLinear)
+
+		return outLinear, outDistance
+
+	# def _initialize_weights(self):
+	# 	for m in self.modules():
+	# 		if isinstance(m, nn.Conv2d):
+	# 			nn.init.kaiming_normal_(m.weight, mode = 'fan_out', nonlinearity = 'relu')
+	# 			if m.bias is not None:
+	# 				nn.init.constant_(m.bias, 0)
+	# 		elif isinstance(m, nn.BatchNorm2d):
+	# 			nn.init.constant_(m.weight, 1)
+	# 			nn.init.constant_(m.bias, 0)
+	# 		elif isinstance(m, nn.Linear):
+	# 			nn.init.normal_(m.weight, 0, 0.01)
+	# 			nn.init.constant_(m.bias, 0)
+
+	def set_anchors(self, means):
+		self.anchors = nn.Parameter(means.double(), requires_grad = False)
+		self.cuda()
+
+	def distance_classifier(self, x):
+		''' Calculates euclidean distance from x to each class anchor
+			Returns n x m array of distance from input of batch_size n to anchors of size m
+		'''
+
+		n = x.size(0)
+		m = self.num_classes
+		d = self.num_classes
+
+		x = x.unsqueeze(1).expand(n, m, d).double()
+		anchors = self.anchors.unsqueeze(0).expand(n, m, d)
+		dists = torch.norm(x-anchors, 2, 2)
+
+		return dists
+
+class BaseEncoder(nn.Module):
+    def __init__(self, init_weights, **kwargs): 
+        super().__init__()
+
+        self.conv1 = nn.Conv1d(in_channels=1, out_channels=32, kernel_size=16, stride=1, padding='valid')
+        self.conv2 = nn.Conv1d(in_channels=32, out_channels=32, kernel_size=16, stride=1, padding='valid')
+        self.conv3 = nn.Conv1d(in_channels=32, out_channels=32, kernel_size=16, stride=1, padding='valid')
+
+        self.drop1 = nn.Dropout(p=0.3)
+
+        self.maxpool = nn.MaxPool1d(kernel_size=8, stride=4, padding=0)
+
+        self.relu = nn.ReLU()
+
+        self.flat = nn.Flatten()
+
+        
+
+        
+
+        self.drop2 = nn.Dropout(p=0.3)
+        self.drop3 = nn.Dropout(p=0.5)
+
+        self.dense1 = nn.Linear(3584, 64)
+
+
+
+        self.encoder1 = nn.Sequential(
+                            self.conv1,
+                            self.relu,
+                            self.conv2,
+                            self.relu,
+                            self.drop1,
+                            self.conv3,
+                            self.relu,
+                            self.drop2,
+                            self.drop1,
+                            self.maxpool,
+                            self.relu,
+                            self.drop2,
+
+                        )
+
+        self.encoder2 = nn.Sequential(
+                                self.flat,
+                                self.dense1,
+                                self.relu,
+                                self.drop3,
+                            )
+
+        
+
+        # if init_weights:
+        #     self._initialize_weights()
+    
+        self.cuda()
+
+
+    # def _initialize_weights(self):
+    #     for m in self.modules():
+    #         if isinstance(m, nn.Conv2d):
+    #             nn.init.kaiming_normal_(m.weight, mode = 'fan_out', nonlinearity = 'relu')
+    #             if m.bias is not None:
+    #                 nn.init.constant_(m.bias, 0)
+    #         elif isinstance(m, nn.BatchNorm2d):
+    #             nn.init.constant_(m.weight, 1)
+    #             nn.init.constant_(m.bias, 0)
+    #         elif isinstance(m, nn.Linear):
+    #             nn.init.normal_(m.weight, 0, 0.01)
+    #             nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        x1 = self.encoder1(x)
+        x2 = self.encoder2(x1)
+        return x2
+
+
+
+
+
+
+
